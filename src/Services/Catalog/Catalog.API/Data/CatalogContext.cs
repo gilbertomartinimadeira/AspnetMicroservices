b3d88b06683d5c8f58fb4bded643b74a0f43cf21
Gilberto Martini Madeira <gilberto.madeira@outlook.com>
2024-01-24 13:40:53 -0300

    creates initial seed

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,20 @@
+using Catalog.API.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data;
+
+public class CatalogContext : ICatalogContext
+{
+    public CatalogContext(IConfiguration configuration)
+    {
+        var client = new MongoClient(configuration["DatabaseSettings:ConnectionString"]);
+
+        var database = client.GetDatabase(configuration["DatabaseSettings:DatabaseName"]);
+
+        Products = database.GetCollection<Product>("DatabaseSettings:CollectionName");
+
+        CatalogContextSeed.SeedData(Products);
+
+    }
+    public IMongoCollection<Product> Products { get; }
+}
