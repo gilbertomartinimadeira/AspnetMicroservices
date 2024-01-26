9d636186ee801f5140fc40a1e433fcfc7331bc13
Gilberto Martini Madeira <gilberto.madeira@outlook.com>
2024-01-26 13:18:05 -0300

    implemnets redis repository for caching

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,11 +1,17 @@
 var builder = WebApplication.CreateBuilder(args);
 
+var configuration = builder.Configuration;
+
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddStackExchangeRedisCache(options => {
+    options.Configuration = configuration["CacheSettings:ConnectionString"];
+});
 
 var app = builder.Build();
 
